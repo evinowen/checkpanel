b932750fe61b85778143970f669d0d81d9735933
Evin Owen <github@evinowen.net>
2020-07-09 12:41:29 -0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace checkpanel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# checkpanel
+
+An attempt at a sort of automated to-do list tracker, built in ASP.NET MVC.
