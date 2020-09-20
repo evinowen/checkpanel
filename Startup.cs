5c938cded4e8133bd69d3f5597a4f9517eb1117b
Evin Owen <github@evinowen.net>
2020-09-19 17:43:00 -0700

    Setup base migrations for data schema

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace checkpanel
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SqlServerContext context)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +46,8 @@ namespace checkpanel
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            context.Database.Migrate();
 
             RewriteOptions options = new RewriteOptions()
                 .AddRedirect(@"^$", "/List");
