518f00faa46488278583ee3536261d49dd30fdce
Evin Owen <github@evinowen.net>
2021-04-06 09:44:50 -0700

    Change login from text-based OTP to TOTP

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
+using OtpNet;
 
 namespace checkpanel.Controllers
 {
@@ -35,46 +36,16 @@ namespace checkpanel.Controllers
             return View(model);
         }
 
-        [HttpPost("generate")]
-        public async Task<IActionResult> Generate()
-        {
-
-            Random generator = new Random();
-            string authentication = generator.Next(0, 999999).ToString("D6");
-
-            HttpContext.Session.SetString("State", "Generated");
-            HttpContext.Session.SetString("Authentication", authentication);
-
-            var twilio_telephone_to = _configuration["TWILIO_TELEPHONE_TO"];
-
-            var queue_connection_string = _configuration["AZURE_QUEUE_CONNECTION"];
-            var queue_name = _configuration["AZURE_QUEUE_SEND_AUTHENTICATION_CODE"];
-
-            QueueClient queue = new QueueClient(queue_connection_string, queue_name);
-
-            var data = new SendAuthenticationCodeModel
-            {
-                Authentication = authentication,
-                Telephone = twilio_telephone_to
-            };
-
-            var options = new JsonSerializerOptions
-            {
-                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-            };
-
-            var json = JsonSerializer.Serialize(data, options);
-            Message message = new Message(Encoding.ASCII.GetBytes(json));
-
-            await queue.SendAsync(message);
-
-            return LocalRedirect("/Login");
-        }
-
         [HttpPost("validate")]
         public IActionResult Validate(string authentication)
         {
-            if (authentication == HttpContext.Session.GetString("Authentication"))
+            var secret = _configuration["TOTP_SECRET"];
+
+            var base32Bytes = Base32Encoding.ToBytes(secret);
+            var totp = new Totp(base32Bytes);
+            var code = totp.ComputeTotp();
+
+            if (authentication == code)
             {
                 HttpContext.Session.SetString("State", "Authenticated");
                 return LocalRedirect("/List");
