141d9c2c4ecf5dd443f2b418e528bece3b03ff22
Evin Owen <github@evinowen.net>
2021-04-07 15:53:18 -0700

    Release 0.0.7 #8
    * Updated Daily Report generation process to allow more detail in report

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -31,6 +31,13 @@ namespace checkpanel.Controllers
             public string EmailAddress { get; set; }
             public int PointsEarned { get; set; }
             public int PointsAvailable { get; set; }
+            public List<SendDailyReportModelRecord> Records { get; set; }
+        }
+
+        public class SendDailyReportModelRecord
+        {
+            public string Name { get; set; }
+            public List<string> Punches { get; set; }
         }
 
         private readonly IConfiguration _configuration;
@@ -47,8 +54,8 @@ namespace checkpanel.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(Report report)
         {
-            var sendgrid_email_address_to = _configuration["SENDGRID_EMAIL_ADDRESS_TO"];
-            var sendgrid_name = _configuration["SENDGRID_NAME"];
+            var report_to_email_address = _configuration["REPORT_TO_EMAIL_ADDRESS"];
+            var report_to_name = _configuration["REPORT_TO_NAME"];
 
             string queue_connection_string = _configuration["AZURE_QUEUE_CONNECTION"];
             string queue_name;
@@ -65,53 +72,136 @@ namespace checkpanel.Controllers
                 case "daily":
                     var points_earned = 0;
                     var points_available = 0;
+                    var records = new List<SendDailyReportModelRecord>();
 
-                    var yesterday = DateTime.Now.AddDays(-1);
-
-                    var items = _context.Items.Where(i => i.DeletedAt == null);
-
-                    switch (yesterday.DayOfWeek)
                     {
-                        case DayOfWeek.Sunday:
-                            items = items.Where(i => i.Sunday == true);
-                            break;
-                        case DayOfWeek.Monday:
-                            items = items.Where(i => i.Monday == true);
-                            break;
-                        case DayOfWeek.Tuesday:
-                            items = items.Where(i => i.Tuesday == true);
-                            break;
-                        case DayOfWeek.Wednesday:
-                            items = items.Where(i => i.Wednesday == true);
-                            break;
-                        case DayOfWeek.Thursday:
-                            items = items.Where(i => i.Thursday == true);
-                            break;
-                        case DayOfWeek.Friday:
-                            items = items.Where(i => i.Friday == true);
-                            break;
-                        case DayOfWeek.Saturday:
-                            items = items.Where(i => i.Sunday == true);
-                            break;
-                    }
+                        var yesterday = DateTime.Now.AddDays(-1);
 
-                    foreach (var item in items.ToList())
-                    {
-                        points_available++;
+                        var items = _context.Items.Where(i => i.DeletedAt == null);
 
-                        var punches = _context.Entry(item).Collection(p => p.Punches).Query().Where(p => p.CreatedAt.Date == yesterday.Date).ToList();
-
-                        if (punches.Count > 0)
+                        switch (yesterday.DayOfWeek)
                         {
-                            points_earned++;
+                            case DayOfWeek.Sunday:
+                                items = items.Where(i => i.Sunday == true);
+                                break;
+                            case DayOfWeek.Monday:
+                                items = items.Where(i => i.Monday == true);
+                                break;
+                            case DayOfWeek.Tuesday:
+                                items = items.Where(i => i.Tuesday == true);
+                                break;
+                            case DayOfWeek.Wednesday:
+                                items = items.Where(i => i.Wednesday == true);
+                                break;
+                            case DayOfWeek.Thursday:
+                                items = items.Where(i => i.Thursday == true);
+                                break;
+                            case DayOfWeek.Friday:
+                                items = items.Where(i => i.Friday == true);
+                                break;
+                            case DayOfWeek.Saturday:
+                                items = items.Where(i => i.Sunday == true);
+                                break;
+                        }
+
+
+                        foreach (var item in items.ToList())
+                        {
+                            points_available++;
+
+                            var punches = _context.Entry(item).Collection(p => p.Punches).Query().Where(p => p.CreatedAt.Date == yesterday.Date).ToList();
+
+                            if (punches.Count > 0)
+                            {
+                                points_earned++;
+                            }
                         }
                     }
 
-                    var data = new SendDailyReportModel{
-                        Name = sendgrid_name,
-                        EmailAddress = sendgrid_email_address_to,
+                    {
+                        var items = _context.Items.OrderBy(i => i.DueHour).ThenBy(i => i.DueMinute).Where(i => i.DeletedAt == null);
+                        foreach (var item in items.ToList())
+                        {
+                            var record = new SendDailyReportModelRecord
+                            {
+                                Name = item.Summary,
+                                Punches = new List<string>() { "free", "free", "free", "free", "free", "free", "free" }
+                            };
+
+                            for (var i = 0; i < 7; i++)
+                            {
+                                var date = DateTime.Now.AddDays(-7 + i).Date;
+
+                                switch (date.DayOfWeek)
+                                {
+                                    case DayOfWeek.Sunday:
+                                        if (item.Sunday == true)
+                                        {
+                                            record.Punches[i] = "miss";
+                                        }
+                                        break;
+                                    case DayOfWeek.Monday:
+                                        if (item.Monday == true)
+                                        {
+                                            record.Punches[i] = "miss";
+                                        }
+                                        break;
+                                    case DayOfWeek.Tuesday:
+                                        if (item.Tuesday == true)
+                                        {
+                                            record.Punches[i] = "miss";
+                                        }
+                                        break;
+                                    case DayOfWeek.Wednesday:
+                                        if (item.Wednesday == true)
+                                        {
+                                            record.Punches[i] = "miss";
+                                        }
+                                        break;
+                                    case DayOfWeek.Thursday:
+                                        if (item.Thursday == true)
+                                        {
+                                            record.Punches[i] = "miss";
+                                        }
+                                        break;
+                                    case DayOfWeek.Friday:
+                                        if (item.Friday == true)
+                                        {
+                                            record.Punches[i] = "miss";
+                                        }
+                                        break;
+                                    case DayOfWeek.Saturday:
+                                        if (item.Saturday == true)
+                                        {
+                                            record.Punches[i] = "miss";
+                                        }
+                                        break;
+                                }
+
+                                if (record.Punches[i] == "miss")
+                                {
+                                    var punches = _context.Entry(item).Collection(p => p.Punches).Query().OrderBy(p => p.CreatedAt).Where(p => p.CreatedAt.Date == date).ToList();
+
+                                    if (punches.Count > 0)
+                                    {
+                                        record.Punches[i] = "hit";
+                                    }
+
+                                }
+
+                            }
+
+                            records.Add(record);
+                        }
+                    }
+
+                    var data = new SendDailyReportModel
+                    {
+                        Name = report_to_name,
+                        EmailAddress = report_to_email_address,
                         PointsEarned = points_earned,
-                        PointsAvailable = points_available
+                        PointsAvailable = points_available,
+                        Records = records
                     };
 
                     queue_name = _configuration["AZURE_QUEUE_SEND_DAILY_REPORT"];
